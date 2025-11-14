d4bd5c7a8bb2bf5cb53e6a6bf8d31be6e8186991
tesuji <taolzu@gmail.com>
2025-11-14 10:49:05 +0000

    use bash array

--- pwnkernel/build.sh
+++ pwnkernel/build.sh
@@ -1,3 +1,7 @@
 #!/bin/bash
 
-docker buildx build --platform linux/amd64 -t tesuji777/pwnkernel --push .
+extra_args=()
+if [ "push" == "$1" ]; then
+  extra_args+=( "--push" )
+fi
+docker buildx build --platform linux/amd64 -t tesuji777/pwnkernel "${extra_args[@]}" .
